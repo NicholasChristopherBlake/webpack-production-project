2a2d1538c4bbfc2a732fe910d050adb1f2167e8d
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-01-24 23:50:10 +0600

    added routing, i18n, theme switcher, language switcher

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { FC, useState } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import { ThemeSwitcher } from "features/ThemeSwitcher";
+import { LangSwitcher } from "features/LangSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <button onClick={onToggle}>Toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
+      </div>
+    </div>
+  );
+};
