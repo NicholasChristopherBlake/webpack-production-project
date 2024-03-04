238b6d19d902c926dc77b37ea2702313258fe1f2
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-03-04 20:44:36 +0500

    fixed api instance

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,6 +5,7 @@ export const $api = axios.create({
   baseURL: __API__,
 });
 
+// For correct work with Server when first loading user data
 $api.interceptors.request.use((config) => {
   if (config.headers) {
     config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
