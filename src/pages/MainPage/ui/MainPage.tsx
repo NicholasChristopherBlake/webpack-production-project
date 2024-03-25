3f202c76fa463602d9ca1b7bf7823f0a9a4f457f
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-03-25 23:39:04 +0500

    rating card fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,6 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { t } from 'i18next';
 import { Page } from '@/widgets/Page';
-import { RatingCard } from '@/entity/Rating';
 
 const MainPage = memo(() => {
   const { t } = useTranslation();
@@ -10,11 +8,6 @@ const MainPage = memo(() => {
   return (
     <Page>
       {t('Главная страница')}
-      <RatingCard
-        feedbackTitle="Please leave feedback about this article"
-        hasFeedback
-        title="What do you think about this article?"
-      />
     </Page>
   );
 });
