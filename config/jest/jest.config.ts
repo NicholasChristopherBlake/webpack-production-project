e594729c6c6e0fffc8645aaf16281e45c90ab637
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-04-02 15:54:08 +0500

    added report for unit testing to CI

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -52,7 +52,7 @@ const config = {
     ["jest-html-reporters", {
       publicPath: "<rootDir>/reports/unit",
       filename: "report.html",
-      openReport: true,
+      // openReport: true,
       inlineSource: true, // no extra JS files
     }],
   ],
