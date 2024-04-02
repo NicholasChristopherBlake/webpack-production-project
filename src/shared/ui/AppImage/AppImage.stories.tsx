137ffdd5d9ea64fe566340ba6ff91b9994b98fff
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-04-02 17:33:34 +0500

    updated loki references

--- src/shared/ui/AppImage/AppImage.stories.tsx
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { AppImage } from './AppImage';
+import { Skeleton } from '../Skeleton';
 
 const meta: Meta<typeof AppImage> = {
   title: 'shared/AppImage',
@@ -12,5 +13,21 @@ const meta: Meta<typeof AppImage> = {
 export default meta;
 type Story = StoryObj<typeof AppImage>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    src: 'https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg',
+    alt: 'image',
+    height: 200,
+    width: 200,
+    fallback: <Skeleton width={200} height={200} />,
+    errorFallback: <div>Error</div>,
+  },
+};
+
+export const ErrorState: Story = {
+  args: {
+    alt: 'image',
+    errorFallback: <div>Error</div>,
+  },
+};
 
