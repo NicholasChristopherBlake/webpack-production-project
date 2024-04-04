312c1e698328b0e0c44a83f769718448ce65923c
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-04-04 09:10:53 +0500

    add https to json server

--- json-server/index.js
+++ json-server/index.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const https = require('https');
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+  cert: fs.readFileSync(
+    path.resolve(__dirname, 'cert.pem'),
+  ),
+};
 
 const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+const router = jsonServer.router(
+  path.resolve(__dirname, 'db.json'),
+);
 
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
@@ -23,18 +33,27 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
   try {
     const { username, password } = req.body;
-    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+    const db = JSON.parse(
+      fs.readFileSync(
+        path.resolve(__dirname, 'db.json'),
+        'UTF-8',
+      ),
+    );
     const { users = [] } = db;
 
     const userFromBd = users.find(
-      (user) => user.username === username && user.password === password,
+      (user) =>
+        user.username === username &&
+        user.password === password,
     );
 
     if (userFromBd) {
       return res.json(userFromBd);
     }
 
-    return res.status(403).json({ message: 'User not found' });
+    return res
+      .status(403)
+      .json({ message: 'User not found' });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: e.message });
@@ -55,6 +74,7 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-server.listen(8000, () => {
-  console.log('server is running on 8000 port');
+const httpsServer = https.createServer(options, server);
+server.listen(443, () => {
+  console.log('server is running on 443 port');
 });
