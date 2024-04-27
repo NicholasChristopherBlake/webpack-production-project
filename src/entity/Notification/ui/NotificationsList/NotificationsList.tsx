3418c016ad85e1dfd322ea1c18c00c5623256a3d
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-04-27 12:09:16 +0500

    Minor fix. NotificationsList

--- src/entity/Notification/ui/NotificationsList/NotificationsList.tsx
+++ src/entity/Notification/ui/NotificationsList/NotificationsList.tsx
@@ -12,7 +12,6 @@ interface NotificationsListProps {
   className?: string;
 }
 
-// TODO странный hover возникает при наведении на notifications
 export const NotificationsList = memo((props: NotificationsListProps) => {
   const { className } = props;
   const { data, isLoading } = useNotifications(null, {
