a16c3294a935afaed5ec79272f00f060951558e0
NicholasChristopherBlake <110085788+NicholasChristopherBlake@users.noreply.github.com>
2024-04-28 14:12:30 +0500

    Minor fix. New user default design - new

--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -1,9 +1,16 @@
 import { LOCAL_STORAGE_LAST_DESIGN_KEY } from '@/shared/const/localStorage';
 import { FeatureFlags } from '@/shared/types/featureFlags';
 
+// By default guests have new design on
+function getDefaultDesign() {
+  if (!localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY)) {
+    return true;
+  }
+  return localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === 'new';
+}
+
 const defaultFeatures: FeatureFlags = {
-  isAppRedesigned:
-    localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === 'new',
+  isAppRedesigned: getDefaultDesign(),
 };
 // add default value {}
 // because might be crashing if feature is undefined
